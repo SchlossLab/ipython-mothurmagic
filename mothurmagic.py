23d64e4c179fb4c68549ef8097a26129c49d515b
Richard Campen <richard@campen.co>
2016-03-14 16:45:11 -0400

    Get and set current file variables using mothurs set.current and get.current instead of manually parsing.

--- mothurmagic.py
+++ mothurmagic.py
@@ -4,7 +4,6 @@ import json
 import os
 import subprocess as sub
 import random
-import re
 from IPython.core.magic import Magics, magics_class, line_magic, cell_magic, line_cell_magic
 
 
@@ -34,25 +33,22 @@ class MothurMagic(Magics):
         """
 
         # save notebooks variables so we have access to them
-        user_ns = self.shell.user_ns.copy()
-        self.local_ns = user_ns
+        self.local_ns = self.shell.user_ns.copy()
 
-        # Load _mothur_current from user_ns if it exists. If it does not exist try loading from file.
+        # Load _mothur_current from the local_ns if it exists. If it does not exist try loading from file.
         # If file does not exist assume this is the first time %%mothur has been run in this notebook
-        # and create the entry in user_ns.
-        if '_mothur_current' in user_ns:
-            _mothur_current = user_ns['_mothur_current']
-        else:
+        # and create the entry in the local ns.
+        if '_mothur_current' not in self.local_ns:
             try:
                 with open('mothur.current.json', 'r') as in_handle:
                     _mothur_current = json.load(in_handle)
-                    user_ns['_mothur_current'] = _mothur_current
+                    self.local_ns['_mothur_current'] = _mothur_current
             except IOError as e:
                 # file doesn't exist or can't be read so create new user_ns entry for current
                 #TODO differentiate permission error from file does not exist error.
                 print('Couldn\'t load _mothur.current variables from file: ', e.args[1])
                 _mothur_current = {}
-                user_ns['_mothur_current'] = _mothur_current
+                self.local_ns['_mothur_current'] = _mothur_current
 
         self.number = line
         self.code = cell
@@ -65,9 +61,8 @@ class MothurMagic(Magics):
         output_files = parse_output(output)
 
         # update _mothur_current and push to notebook environment
-        _mothur_current.update(output_files)
-        user_ns['_mothur_current'].update(_mothur_current)
-        self.shell.user_ns.update(user_ns)
+        self.local_ns['_mothur_current'].update(output_files)
+        self.shell.user_ns.update(self.local_ns)
 
         # create/overwrite mothur.current.json with contents of _mothur_current
         # from current notebook environment.
@@ -76,51 +71,22 @@ class MothurMagic(Magics):
                 json.dump(self.shell.user_ns['_mothur_current'], out_handle)
         except IOError as e:
             #TODO differentiate permission error from file does not exist error.
-            print('Couldn\'t save mothur_current variables to file: ', e.ars[1])
+            print('Couldn\'t save _mothur_current variables to file: ', e.ars[1])
 
         display_output(output)
 
 
 def parse_input(self):
-    """Parse commands and insert local variables.
+    """Parse commands and insert current variables form local namespace.
 
-    Arguments:
-        - code: mothur commands entered in the notebook cell
+    Prepends commands with set.commands to set mothur current varibales with variables from
+    local namespace. Appends get.current to end of commands so that output will contain new
+    current variables for parsing."""
 
-    Split the commands (if more than one) and parse the first command, replacing 'current' selections
-    with the appropriate file path specified in _mothur_current. Recombine the commands and return.
-    """
-
-    commands = self.code.split("\n")
-
-    new_commands = []
-    for idx, command in enumerate(commands):
-        # parse first command
-        if idx == 0:
-            first_command = command
-            split_command = (re.split('\(|\)', first_command))
-            mothur_command = split_command[0]
-            command_arguments = split_command[1]
-
-            # parse command arguments
-            new_arguments = []
-            for argument in command_arguments.split(', '):
-                split_argument = argument.split('=')
-                argument_type = split_argument[0]
-                argument_argument = split_argument[1]
-                if argument_argument.lower() == 'current':
-                    _mothur_current = self.local_ns['_mothur_current']
-                    argument_argument = _mothur_current[argument_type]
-                    new_argument = '%s=%s' % (argument_type, argument_argument)
-                    new_arguments.append(new_argument)
-                else:
-                    new_arguments.append(argument)
-            new_arguments = ', '.join(new_arguments)
-            new_command = '%s(%s)' % (mothur_command, new_arguments)
-            new_commands.append(new_command)
-        else:
-            new_commands.append(command)
-
+    new_commands = [command for command in self.code.split("\n")]
+    current_vars = ', '.join(['%s=%s' % (k, v) for k,v in self.local_ns['_mothur_current'].items()])
+    new_commands.insert(0, 'set.current(%s)' % current_vars)
+    new_commands.append('get.current()')
     return new_commands
 
 
@@ -159,21 +125,22 @@ def parse_output(logfile):
 
     with open(logfile, 'r') as log:
         lines = (line for line in log.readlines())
-    output_files = {}
+    current_files = {}
     for line in lines:
-        if "Output File Names:" in line:
+        if "Current files saved by mothur:" in line:
             while True:
                 try:
-                    filename = (next(line for line in lines)).split()
-                    if filename:
-                        filetype = filename[0].split('.')[-1]
-                        output_files[filetype] = filename[0]
+                    output_file = (next(line for line in lines)).split()
+                    if output_file:
+                        file_type = output_file[0].split('=')[0]
+                        file_name = output_file[0].split('=')[1]
+                        current_files[file_type] = file_name
                     else:
                         break
                 except StopIteration:
                     break
 
-    return output_files
+    return current_files
 
 
 def display_output(logfile):
